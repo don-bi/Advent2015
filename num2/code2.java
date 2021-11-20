e56e0d0f7b672f4d9e887ad0afa48cfce5716480
don-bi <dbi30@stuy.edu>
2021-11-19 20:58:40 -0500

    got 1 star

--- num2/code2.java
+++ num2/code2.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.Arrays;
 
-public class code {
+public class code2 {
     public static void main(String[] args) throws Exception{
         run();
     }
@@ -9,32 +9,41 @@ public class code {
     public static void run() throws Exception{
         BufferedReader br = new BufferedReader(new FileReader("data.txt"));
         String line;
-        int totalpaper = 0;
+        int ribbon = 0;
         while ((line = br.readLine()) != null){
             String[] dimenString = line.split("x");
             int[] dimensions = new int[dimenString.length];
             for (int i = 0; i < dimenString.length; i ++){
                 dimensions[i] = Integer.parseInt(dimenString[i]);
             }
-            int s1 = dimensions[0] * dimensions[1];
-            int s2 = dimensions[1] * dimensions[2];
-            int s3 = dimensions[2] * dimensions[0];
-            int[] sides = {s1, s2, s3};
-            int min = min(sides);
-            totalpaper += 2 * s1 + 2 * s2 + 2 * s3 + min;
+            int perim = ribbon(dimensions);
+            //System.out.println(perim);
+            ribbon += perim + dimensions[0] * dimensions[1] * dimensions[2];
+            //System.out.println(perim + dimensions[0] * dimensions[1] * dimensions[2]);
         }
-        System.out.println(totalpaper);
+        System.out.println(ribbon);
         br.close();
     }
 
-    public static int min(int[] data){
-        int min = data[0];
+    public static int max(int[] data){
+        int max = data[0];
         for (int i = 0; i < data.length; i ++){
-            if (data[i] < min){
-                min = data[i];
+            if (data[i] > max){
+                max = data[i];
             }
         }
-        return min;
+        return max;
+    }
+
+    public static int ribbon(int[] data){
+        int max = max(data);
+        int halfperim = 0;
+        for (int i = 0; i < data.length; i ++){
+            if (data[i] != max){
+                halfperim += data[i];
+            }
+        }
+        return halfperim * 2;
     }
 
     public static String arr2String(int[] arr){
